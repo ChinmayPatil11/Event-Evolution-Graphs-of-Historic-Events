b654223e1bac70fa9f50a33c37d784472afb4aef
Chinmay Patil <chinmaypatil2000@gmail.com>
2021-11-01 20:03:18 +0530

    Updated files

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,9 +1,6 @@
-import pandas as pd
-
-
 def remove_extra_entities(text):
     ret = []
-    longest = max(text,key=len)
+    longest = max(text, key=len)
     ret.append(longest)
     for i in text:
         if i not in longest:
@@ -25,7 +22,7 @@ def clean_df(df):
     df['subject'] = df['subject'].apply(lambda x: remove_extra_entities(x))
     df['year'] = df['year'].str.rstrip(':')
     #print(df['year'])
-    new_columns = df['year'].str.split('–',n=1,expand=True)
+    new_columns = df['year'].str.split('–', n=1, expand=True)
     df['year from'] = new_columns[0]
     df['year to'] = new_columns[1]
     #print(df.columns)
